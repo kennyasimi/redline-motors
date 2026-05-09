ac862959be2cc17ae5daf12a2d7fab322f1752a8
kennyasimi <kennyasimi23@gmail.com>
2026-05-09 13:25:16 +0300

    minor cleanup

--- api-project/api/index.php
+++ api-project/api/index.php
@@ -4,8 +4,3 @@ header('Content-Type: application/json');
 require_once __DIR__ . '/routes.php';
 
 
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH');
-header('Access-Control-Allow-Headers: Content-Type');
